4741a9e72c9e6f3e0a6563bacffa4ec031814b3e
Thulkrast <maciej.plonka@hotmail.com>
2020-06-14 20:51:19 +0200

    Moved player's files to Player folder, added dash ability

---
+++ Player/Input Provider.gd
@@ -0,0 +1,45 @@
+extends Node
+
+
+signal input_direction(input_direction)
+signal dash
+const UP = "ui_up"
+const DOWN = "ui_down"
+const LEFT = "ui_left"
+const RIGHT = "ui_right"
+const DASH = "ui_dash"
+
+func _get_direction(negative: String, positive: String) -> int:
+	if(Input.is_action_pressed(positive)):
+		return 1
+	if(Input.is_action_pressed(negative)):
+		return -1
+	return 0
+
+func _get_vertical_direction() -> int:
+	return _get_direction(UP, DOWN)
+	
+func _get_horizontal_direction() -> int:
+	return _get_direction(LEFT, RIGHT)	
+
+func _get_input_direction() -> Vector2:
+	return Vector2(_get_horizontal_direction(), _get_vertical_direction())
+
+func _handle_input_direction(delta:float): 
+	var input_direction = _get_input_direction()
+	if input_direction == Vector2.ZERO:
+		return
+	emit_signal('input_direction', input_direction.normalized() * delta)
+
+
+func _handle_dash_input(event: InputEvent):
+	if event.is_action_pressed(DASH):
+		emit_signal("dash")
+	
+func _input(event): 
+		_handle_dash_input(event)
+	
+func _process(delta):
+	_handle_input_direction(delta)
+
+
--- Player/Player.gd
+++ Player/Player.gd
@@ -4,27 +4,30 @@ extends KinematicBody2D
 export(int) var top_speed = 250
 export(int) var steps_to_reach_topspeed = 3
 export(float) var drag_force = 0.25
+const DASH_FORCE = 0.25
 
 const MAGIC_NUMBER = 60
 #variables 
 var acceleration = Vector2.ZERO
 var velocity = Vector2.ZERO
+var speed = top_speed / steps_to_reach_topspeed * MAGIC_NUMBER
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$InputProvider.connect('input_direction',self, "_on_input_direction")
-	pass # Replace with function body.
+	var input_provider = $InputProvider
+	input_provider.connect('input_direction',self, "_on_input_direction")
+	input_provider.connect('dash',self,'_on_dash')
 
 func _on_input_direction(input_direction:Vector2):
-	acceleration += input_direction
+	acceleration += input_direction * speed
+
+func _on_dash():
+	acceleration += acceleration.normalized() * speed 
 
 func _calculate_drag() -> Vector2:
 	return velocity * drag_force 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var drag = _calculate_drag()
-	velocity += (acceleration * top_speed / steps_to_reach_topspeed * MAGIC_NUMBER)
-	velocity -= drag
-	velocity = velocity.clamped(top_speed)
+	velocity += acceleration - _calculate_drag()
 	velocity = move_and_slide(velocity)
 	acceleration = Vector2.ZERO
