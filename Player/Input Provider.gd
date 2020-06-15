0b1e22e2b8953afce0172a47ee7e1d65e5b8a0fd
Thulkrast <maciej.plonka@hotmail.com>
2020-06-15 08:16:53 +0200

    Fixed formatting

--- Player/Input Provider.gd
+++ Player/Input Provider.gd
@@ -3,11 +3,13 @@ extends Node
 
 signal input_direction(input_direction)
 signal dash
+
 const UP = "ui_up"
 const DOWN = "ui_down"
 const LEFT = "ui_left"
 const RIGHT = "ui_right"
 const DASH = "ui_dash"
+
 
 func _get_direction(negative: String, positive: String) -> int:
 	if(Input.is_action_pressed(positive)):
@@ -15,15 +17,21 @@ func _get_direction(negative: String, positive: String) -> int:
 	if(Input.is_action_pressed(negative)):
 		return -1
 	return 0
+	
 
 func _get_vertical_direction() -> int:
 	return _get_direction(UP, DOWN)
-	
+
+
 func _get_horizontal_direction() -> int:
 	return _get_direction(LEFT, RIGHT)	
 
+
 func _get_input_direction() -> Vector2:
-	return Vector2(_get_horizontal_direction(), _get_vertical_direction())
+	var horizontal = _get_horizontal_direction()
+	var vertical = _get_vertical_direction()
+	return Vector2(horizontal, vertical)
+
 
 func _handle_input_direction(delta:float): 
 	var input_direction = _get_input_direction()
@@ -35,10 +43,12 @@ func _handle_input_direction(delta:float):
 func _handle_dash_input(event: InputEvent):
 	if event.is_action_pressed(DASH):
 		emit_signal("dash")
-	
+
+
 func _input(event): 
 		_handle_dash_input(event)
-	
+
+
 func _process(delta):
 	_handle_input_direction(delta)
 
--- Player/Player.gd
+++ Player/Player.gd
@@ -12,19 +12,24 @@ var acceleration = Vector2.ZERO
 var velocity = Vector2.ZERO
 var speed = top_speed / steps_to_reach_topspeed * MAGIC_NUMBER
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	var input_provider = $InputProvider
 	input_provider.connect('input_direction',self, "_on_input_direction")
 	input_provider.connect('dash',self,'_on_dash')
 
+
 func _on_input_direction(input_direction:Vector2):
 	acceleration += input_direction * speed
+
 
 func _on_dash():
 	acceleration += acceleration.normalized() * speed 
 
+
 func _calculate_drag() -> Vector2:
 	return velocity * drag_force 
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
